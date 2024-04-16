0a9c223f3aa46a151d693c1e95ef1c78c661f4a6
Derek Castillo <castilloderek445@gmail.com>
2024-04-16 18:12:20 -0500

    workflows thingy 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 const LIVE_URL = "https://unspokenswag.github.io"
-const BASE_URL = LIVE_URL 
+const BASE_URL = 'my-repo' 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: BASE_URL,
+  site: "https://unspokenswag.github.io",
+  base: "my-repo",
   output: "static"
 });
