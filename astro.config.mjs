6e18faaaf3b73deb8c9a534a22167afff53d026f
Derek Castillo <castilloderek445@gmail.com>
2024-04-16 17:33:38 -0500

    deploy to github pages stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const LIVE_URL = "https://unspokenswag.github.io"
+const BASE_URL = LIVE_URL 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+  site: BASE_URL
 });
